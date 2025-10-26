630e20d3dca6ed0c0c789325559129eca30ad8d0
Никита <Nikita30042005@yandex.ru>
2025-10-26 15:35:23 +0300

    fix env 2

--- internal/api/server.py
+++ internal/api/server.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-from dotenv import load_dotenv
 from internal.app.handler.handler import register_routes
 
 
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from internal.api.server import run
 
 if __name__ == "__main__":
-    load_dotenv("deploy/.env")
     run()
